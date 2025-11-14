db40a74f83fcb5dda5009f1e6ea00262951bdad9
CESAR GUIJARRO ROSALENY <cguijarro@cip.fpmislata.com>
2025-11-14 13:18:30 +0100

    modificado tests

--- src/test/java/es/cesguiro/persistence/repository/BookRepositoryImplShould.java
+++ src/test/java/es/cesguiro/persistence/repository/BookRepositoryImplShould.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -37,70 +38,40 @@ class BookRepositoryImplShould {
     @InjectMocks
     private BookRepositoryImpl bookRepository;
 
-    @Test
-    public void return_list_of_books_when_page_1_and_pages_more_than_1(){
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 2, 5",
+            "1, 5, 5, 5",
+            "1, 5, 3, 3",
+            "2, 4, 4, 10",
+            "2, 4, 4, 4",
+            "2, 4, 3, 3"
+    })
+    public void return_page_of_bookEntity(int page, int size, int elementsToCreate, long totalElements){
         List<BookJpaEntity> bookJpaEntities = Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL)
-                .size(2)
+                .size(elementsToCreate)
                 .withSeed(20)
                 .create();
         List<BookEntity> expectedBookEntities = bookJpaEntities.stream()
                 .map(BookMapper.INSTANCE::fromBookJpaEntityToBookEntity)
                 .toList();
-        when(bookJpaDao.findAll(1, 2)).thenReturn(bookJpaEntities);
-        when(bookJpaDao.count()).thenReturn(5L);
+        when(bookJpaDao.findAll(anyInt(), anyInt())).thenReturn(bookJpaEntities);
+        when(bookJpaDao.count()).thenReturn(totalElements);
 
-        Page<BookEntity> expected = new Page<>(expectedBookEntities, 1, 2, 5L);
-        Page<BookEntity> result = bookRepository.findAll(1, 2);
+        Page<BookEntity> expected = new Page<>(expectedBookEntities, page, size, totalElements);
+        Page<BookEntity> result = bookRepository.findAll(page, size);
 
         assertThat(result)
                 .usingRecursiveComparison()
                 .isEqualTo(expected);
     }
 
-    static Stream<Arguments> provideFindAllArguments() {
-        return Stream.of(
-                Arguments.of(1, 5, 2L, Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL).size(2).withSeed(10).create()),
-                Arguments.of(1, 10, 2L, Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL).size(2).withSeed(20).create()),
-                Arguments.of(1, 3, 3L, Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL).size(3).withSeed(30).create()),
-                Arguments.of(1, 3, 9L, Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL).size(3).withSeed(40).create()),
-                Arguments.of(2, 3, 5L, Instancio.ofList(InstancioModel.BOOK_JPA_ENTITY_MODEL).size(3).withSeed(50).create())
-        );
-    }
-
-    @ParameterizedTest
-    @DisplayName("findAll should return paginated BookEntity list")
-    @MethodSource("provideFindAllArguments")
-    void findAll_ShouldReturnPaginatedBookEntityList(int pageNumber, int pageSize, long expectedTotalElements, List<BookJpaEntity> bookJpaEntities) {
-        when(bookJpaDao.findAll(anyInt(), anyInt()))
-                .thenReturn(bookJpaEntities);
-        when(bookJpaDao.count())
-                .thenReturn(expectedTotalElements);
-
-        List<BookEntity> bookEntities = bookJpaEntities.stream()
-                .map(BookMapper.INSTANCE::fromBookJpaEntityToBookEntity)
-                .toList();
-        Page<BookEntity> expected = new Page<>(
-                bookEntities,
-                pageNumber,
-                pageSize,
-                expectedTotalElements
-        );
-        Page <BookEntity> result = bookRepository.findAll(pageNumber, pageSize);
-
-        // Assert
-        assertAll(
-                () -> assertNotNull(result, "Resulting Page should not be null"),
-                () -> assertEquals(expected.totalElements(), result.totalElements(), "Total elements should match"),
-                () -> assertEquals(expected.pageSize(), result.pageSize(), "Page size should match"),
-                () -> assertEquals(expected.pageNumber(), result.pageNumber(), "Page number should match"),
-                () -> assertEquals(expected.data().size(), result.data().size(), "Content size should match")
-        );
-    }
 
 
     @Test
     @DisplayName("findByIsbn should return BookEntity when found")
-    void findByIsbn_ShouldReturnBookEntityWhenFound() {
+    void return_optional_bookEntity_when_isbn_exists() {
         BookJpaEntity bookJpaEntity = Instancio.of(InstancioModel.BOOK_JPA_ENTITY_MODEL).create();
 
         when(bookJpaDao.findByIsbn(anyString()))
@@ -109,36 +80,20 @@ class BookRepositoryImplShould {
         Optional<BookEntity> expected = Optional.of(BookMapper.INSTANCE.fromBookJpaEntityToBookEntity(bookJpaEntity));
         Optional<BookEntity> result = bookRepository.findByIsbn("some-isbn");
 
-
-        // Assert
-        assertAll(
-                () -> assertTrue(result.isPresent(), "BookEntity should be present"),
-                () -> assertEquals(expected.get().isbn(), result.get().isbn(), "ISBNs should match"),
-                () -> assertEquals(expected.get().titleEs(), result.get().titleEs(), "TitleEs should match"),
-                () -> assertEquals(expected.get().titleEn(), result.get().titleEn(), "TitleEn should match"),
-                () -> assertEquals(expected.get().synopsisEs(), result.get().synopsisEs(), "SynopsisEs should match"),
-                () -> assertEquals(expected.get().synopsisEn(), result.get().synopsisEn(), "SynopsisEn should match"),
-                () -> assertEquals(expected.get().basePrice(), result.get().basePrice(), "BasePrices should match"),
-                () -> assertEquals(expected.get().discountPercentage(), result.get().discountPercentage(), "DiscountPercentages should match"),
-                () -> assertEquals(expected.get().cover(), result.get().cover(), "Covers should match"),
-                () -> assertEquals(expected.get().publicationDate(), result.get().publicationDate(), "PublicationDates should match"),
-                () -> assertEquals(expected.get().publisher().id(), result.get().publisher().id(), "Publisher IDs should match"),
-                () -> assertEquals(expected.get().authors().size(), result.get().authors().size(), "Number of authors should match"),
-                () -> assertEquals(expected.get().authors().getFirst().id(), result.get().authors().getFirst().id(), "First author IDs should match"),
-                () -> assertEquals(expected.get().authors().getLast().id(), result.get().authors().getLast().id(), "Last author IDs should match")
-        );
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
     }
 
     @Test
     @DisplayName("findByIsbn should return empty when not found")
-    void findByIsbn_ShouldReturnEmptyWhenNotFound() {
+    void return_optional_empty_when_isbn_does_not_exist() {
         when(bookJpaDao.findByIsbn(anyString()))
                 .thenReturn(Optional.empty());
 
         Optional<BookEntity> result = bookRepository.findByIsbn("non-existent-isbn");
 
-        // Assert
-        assertFalse(result.isPresent(), "BookEntity should not be present");
+        assertThat(result).isEmpty();
     }
 
 }
