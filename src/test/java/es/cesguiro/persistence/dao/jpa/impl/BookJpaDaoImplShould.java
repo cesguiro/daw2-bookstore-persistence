3ff484b6af89feaea166fa0c33e1fb418bbfc357
CESAR GUIJARRO ROSALENY <cguijarro@cip.fpmislata.com>
2025-11-21 11:58:03 +0100

    Nuevos tests

--- src/test/java/es/cesguiro/persistence/dao/jpa/impl/BookJpaDaoImplShould.java
+++ src/test/java/es/cesguiro/persistence/dao/jpa/impl/BookJpaDaoImplShould.java
@@ -7,11 +7,12 @@ import es.cesguiro.persistence.dao.jpa.entity.AuthorJpaEntity;
 import es.cesguiro.persistence.dao.jpa.entity.BookJpaEntity;
 import es.cesguiro.persistence.dao.jpa.entity.PublisherJpaEntity;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,28 +27,55 @@ class BookJpaDaoImplShould extends BaseJpaDaoTest<BookJpaDao>{
         PublisherJpaEntity publisherJpaEntity = new PublisherJpaEntity();
         publisherJpaEntity.setId(1L);
 
-        AuthorJpaEntity authorJpaEntity = new AuthorJpaEntity();
-        authorJpaEntity.setId(1L);
+        /*AuthorJpaEntity authorJpaEntity = new AuthorJpaEntity();
+        authorJpaEntity.setId(1L);*/
 
         BookJpaEntity newBook = new BookJpaEntity(
                 null,
-                "9876543210987",
-                "Libro 2",
-                "Book 2",
-                "sinopsis del libro 2",
-                "book 2 synopsis",
+                "1111111111111",
+                "Nuevo libro",
+                "New book",
+                "sinopsis del nuevo libro",
+                "new book synopsis",
                 new BigDecimal("0"),
                 null,
                 null,
                 null,
                 publisherJpaEntity,
-                List.of(authorJpaEntity)
+                List.of()
         );
 
         dao.insert(newBook);
 
         assertThat(newBook.getId()).isNotNull();
         //flushAndCommitForDbRider();
+    }
+
+    @Test
+    void insert_book_with_publication_date_correctly() {
+        PublisherJpaEntity publisherJpaEntity = new PublisherJpaEntity();
+        publisherJpaEntity.setId(1L);
+
+        LocalDate date = LocalDate.now();
+
+        BookJpaEntity newBook = new BookJpaEntity(
+                null,
+                "1111111111111",
+                "Nuevo libro",
+                "New book",
+                "sinopsis del nuevo libro",
+                "new book synopsis",
+                new BigDecimal("0"),
+                null,
+                null,
+                date,
+                publisherJpaEntity,
+                List.of()
+        );
+
+        dao.insert(newBook);
+
+        assertThat(newBook.getId()).isNotNull();
     }
 
     // Testear isbn repetido por concurrencia -> la excepción debería capturarse en un nivel superior (servicio) (ValidationException de domain?)
